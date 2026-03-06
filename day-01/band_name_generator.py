0a6fc95c04ed1a20e7ba2c0571ea31ce462fe57c
Ujjwal Bijalwan <ujjwalbijalwan589@gmail.com>
2026-03-06 15:53:15 +0530

    Create band_name_generator.py
    Added First Python Program Challenge Code.
    Features:
    - Greet the user
    - Take input using input()
    - Print the result(i.e band name) using print()
    - Other things I learnt is to use comments and concatenation using'+'.

---
+++ day-01/band_name_generator.py
@@ -0,0 +1,8 @@
+# 1. Greetings to user of the program
+print("\n\nWelcome to the Band Name Generator! \n")
+# 2. Taking two pieces of information : name of home-town and pet 
+town_name = input("Enter the name of your home-town: \n")
+pet_name = input("\nEnter the name of your pet: \n")
+#Printing the suggested name of band by simple fun logic of combining the two names above
+print("\nYour Band Name could be : \n"+ "The "+ town_name+" "+pet_name)
+#------------FINISH-----------#
